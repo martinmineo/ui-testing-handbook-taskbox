c5a4029f9f0b98a57aa4e6010667f2fa924bd718
Martin Mineo <martinmineo@gmail.com>
2022-01-12 12:14:49 -0300

    bell icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,7 +8,7 @@ import {
   Box,
   VisuallyHidden,
 } from '@chakra-ui/react';
-import { StarIcon } from '@chakra-ui/icons';
+import { BellIcon } from '@chakra-ui/icons';
 
 
 export const Task = ({
@@ -59,7 +59,7 @@ export const Task = ({
       flex="none"
       aria-label={state === 'TASK_PINNED' ? 'unpin' : 'pin'}
       variant={state === 'TASK_PINNED' ? 'unpin' : 'pin'}
-      icon={<StarIcon />}
+      icon={<BellIcon />}
       onClick={() => onTogglePinTask(state, id)}
     />
   </Flex>
